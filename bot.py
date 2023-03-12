c68b5874c7a92e8decfd41d5c310c1bb5459c6d9
joker153 <77005994+joker153@users.noreply.github.com>
2023-03-13 02:17:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,59 +1,89 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import os
 import time
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Your API ID and API hash
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+# Telegram API configuration
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 
-# Your Bot Token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+# Bot configuration
+auth_channel = int(os.environ.get("AUTH_CHANNEL"))
+delete_time = 300  # seconds (5 minutes)
 
-# ID of the authorized channel
-AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", ""))
+# Create a new Pyrogram Client instance
+app = Client(
+    "my_bot",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
+)
 
-# Create a Pyrogram client instance
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-# Function to check if user is authorized to use the bot
-def is_authorized(chat_id):
-    try:
-        member = app.get_chat_member(chat_id=AUTH_CHANNEL, user_id=chat_id)
-        if member.status == 'member' or member.status == 'creator' or member.status == 'administrator':
-            return True
-        else:
-            return False
-    except Exception as e:
-        return False
-
-# Function to delete messages
-def delete_message(client, message):
-    if message.chat.type != 'private' and is_authorized(message.chat.id):
-        message.delete()
-
-# Command to start the bot
+# Handle the "/start" command
 @app.on_message(filters.command("start"))
-def start(client, message):
-    # Check if user is authorized
-    if not is_authorized(message.chat.id):
-        message.reply_text("You are not authorized to use this bot. Please join the authorized channel first.")
-        return
-    # Send a message with instructions
-    message.reply_text("Add me to a group and give me permission to delete messages. I will automatically delete all messages in the group after 5 minutes.")
+def start_command(client: Client, message: Message):
+    # Send a welcome message
+    message.reply_text("Hello! Please add me to a group so I can delete messages there.")
 
-# Handler to delete messages
+# Handle messages in a group chat
 @app.on_message(filters.group & ~filters.edited)
-def delete_messages(client, message):
-    if message.chat.type != 'private' and is_authorized(message.chat.id):
-        # Schedule message deletion after 5 minutes
-        client.scheduler.schedule(300, delete_message, message=message)
+def group_message(client: Client, message: Message):
+    if message.chat.id == auth_channel:
+        # Save the message ID and timestamp for future use
+        message_info = {
+            "chat_id": message.chat.id,
+            "message_id": message.message_id,
+            "timestamp": time.time()
+        }
 
-# Custom delete button in PM settings
+        # Store the message info in a list
+        client._message_infos = client._message_infos[:10] + [message_info]
+
+        # Add a custom "Delete" button to the message
+        button = InlineKeyboardButton(
+            text="Delete",
+            callback_data=f"delete_{message.chat.id}_{message.message_id}"
+        )
+        markup = InlineKeyboardMarkup([[button]])
+
+        # Send the message with the custom button
+        client.send_message(
+            chat_id=message.chat.id,
+            text=message.text,
+            reply_markup=markup
+        )
+
+# Handle button clicks
 @app.on_callback_query()
-def custom_delete_button(client, callback_query):
-    if callback_query.data == "delete":
-        callback_query.message.delete()
+def handle_callback(client: Client, callback_query: Message):
+    if "delete" in callback_query.data:
+        # Get the chat ID and message ID from the callback data
+        chat_id, message_id = map(int, callback_query.data.split("_")[1:3])
+
+        # Check if the user is authorized to delete messages
+        if callback_query.from_user.id != auth_channel:
+            callback_query.answer("You are not authorized to perform this action!")
+            return
+
+        # Delete the message
+        client.delete_messages(chat_id=chat_id, message_ids=message_id)
+
+        # Answer the callback query
+        callback_query.answer("Message deleted successfully!")
+
+# Delete old messages periodically
+@app.on_timer(60)
+def delete_old_messages(client: Client):
+    now = time.time()
+    for message_info in client._message_infos:
+        if now - message_info["timestamp"] > delete_time:
+            # Delete the message
+            client.delete_messages(chat_id=message_info["chat_id"], message_ids=message_info["message_id"])
+
+            # Remove the message info from the list
+            client._message_infos.remove(message_info)
 
 # Start the bot
 app.run()
