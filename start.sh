53c81413df4b7fc3d030f4e324da570f6b349ff9
joker153 <77005994+joker153@users.noreply.github.com>
2023-03-13 00:34:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone  
+  git clone  https://github.com/joker153/auto-deletebot.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /
+  git clone $UPSTREAM_REPO /auto-deletebot
 fi
-cd /Evamari12
+cd /auto-deletebot
 pip freeze > requirements.txt
 echo "Starting Bot...."
 python3 bot.py
