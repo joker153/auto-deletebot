06f7c4762554bde9b9abf53651631bd80e1c67eb
joker153 <77005994+joker153@users.noreply.github.com>
2023-03-13 08:08:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,7 @@ bot_token = os.environ.get("BOT_TOKEN")
 
 # Bot configuration
 auth_channel = int(os.environ.get("AUTH_CHANNEL"))
-default_delete_time = 300  # seconds (5 minutes)
-max_delete_time = 3600  # seconds (1 hour)
+delete_time = 300  # seconds (5 minutes)
 
 # Create a new Pyrogram Client instance
 app = Client(
@@ -26,52 +25,20 @@ app = Client(
 app._message_infos = []
 
 # Handle the "/start" command
-@app.on_message(filters.command("start"))
+@app.on_message(filters.private & filters.command("start"))
 def start_command(client: Client, message: Message):
     # Send a welcome message
-    user_mention = message.from_user.mention if message.from_user.username else message.from_user.first_name
-    message.reply_text(f"Hello {user_mention}! Please add me to a group so I can delete messages there.")
+    message.reply_text(f"Hello {message.from_user.mention}! Please add me to a group so I can delete messages there.")
 
-# Handle the settings command in a private chat
+# Handle the "/settings" command
 @app.on_message(filters.private & filters.command("settings"))
 def settings_command(client: Client, message: Message):
-    # Send a message with the current deletion time and the "Change" button
-    text = f"The current deletion time is {delete_time} seconds."
-    button = InlineKeyboardButton(text="Change", callback_data="change_delete_time")
-    markup = InlineKeyboardMarkup([[button]])
-    message.reply_text(text, reply_markup=markup)
-
-# Handle button clicks
-@app.on_callback_query()
-def handle_callback(client: Client, callback_query: Message):
-    if "delete" in callback_query.data:
-        # Get the chat ID and message ID from the callback data
-        chat_id, message_id = map(int, callback_query.data.split("_")[1:3])
-
-        # Check if the user is authorized to delete messages
-        if callback_query.from_user.id != auth_channel:
-            callback_query.answer("You are not authorized to perform this action!")
-            return
-
-        # Delete the message
-        client.delete_messages(chat_id=chat_id, message_ids=message_id)
-
-        # Answer the callback query
-        callback_query.answer("Message deleted successfully!")
-
-    elif callback_query.data == "change_delete_time":
-        # Send a message asking for the new deletion time
-        callback_query.answer()
-        text = "Enter the new deletion time in seconds (must be between 30 and 300):"
-        client.send_message(chat_id=callback_query.from_user.id, text=text)
-
-        # Save the message ID and timestamp for future use
-        message_info = {
-            "chat_id": callback_query.from_user.id,
-            "message_id": callback_query.message.message_id,
-            "timestamp": time.time()
-        }
-        client._message_infos = client._message_infos[:10] + [message_info]
+    # Send a message with custom deletion time buttons
+    button_1 = InlineKeyboardButton(text="1 minute", callback_data="delete_60")
+    button_2 = InlineKeyboardButton(text="5 minutes", callback_data="delete_300")
+    button_3 = InlineKeyboardButton(text="10 minutes", callback_data="delete_600")
+    markup = InlineKeyboardMarkup([[button_1], [button_2], [button_3]])
+    message.reply_text("Select the custom deletion time:", reply_markup=markup)
 
 # Handle messages in a group chat
 @app.on_message(filters.group & ~filters.edited)
@@ -100,6 +67,27 @@ def group_message(client: Client, message: Message):
             text=message.text,
             reply_markup=markup
         )
+
+# Handle button clicks
+@app.on_callback_query()
+def handle_callback(client: Client, callback_query: Message):
+    if "delete" in callback_query.data:
+        # Get the chat ID and message ID from the callback data
+        chat_id, message_id = map(int, callback_query.data.split("_")[1:3])
+
+        # Get the custom deletion time from the callback data
+        delete_time = int(callback_query.data.split("_")[3])
+
+        # Check if the user is authorized to delete messages
+        if callback_query.from_user.id != auth_channel:
+            callback_query.answer("You are not authorized to perform this action!")
+            return
+
+        # Delete the message
+        client.delete_messages(chat_id=chat_id, message_ids=message_id)
+
+        # Answer the callback query
+        callback_query.answer("Message deleted successfully!")
 
 # Start the bot
 app.start()
